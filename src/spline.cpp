98541c2e6e9ffbdbce23ebbd98be8df84bfb8da5
manuel1801 <manuel.barkey@web.de>
2020-09-22 19:27:56 +0200

    started impl of control points

--- src/spline.cpp
+++ src/spline.cpp
@@ -18,15 +18,65 @@ using namespace std;
 int main()
 {
 
-    double U[16] = {0, 0, 0, 0, 0.11, 0.23, 0.35, 0.48, 0.60, 0.68, 0.77, 0.84, 1, 1, 1, 1};
-    double P[16*3] = {83, 34, -168, 146, -182, -45, 207, 31, -89, -29, 32, 71,
-                      -5, 128, -41, 177, 88, 21, 14, -172, 30, 90,-54, -32,
-                      119, 120,
-                      88, 252, 245, 68, 98, 83, 121, 218, 188, 192};
+    int p = 3, d = 3;
+    int n = 9;
+    int idx;
+
+    double U[16] = {0, 0, 0, 0, 0.11, 0.23, 0.35, 0.48, 0.60, 0.68, 0.77, 0.84, 1.0, 1.0, 1.0, 1.0};
+//    double P[16*3] = {83, 34, -168, 146, -182, -45, 207, 31, -89, -29, 32, 71,
+//                      -5, 128, -41, 177, 88, 21, 14, -172, 30, 90,-54, -32,
+//                      119, 120,
+//                      88, 252, 245, 68, 98, 83, 121, 218, 188, 192};
+
+    //waypoints q
+    double q[10*d] =    {83, -64 ,42 ,-98, -13 ,140, 43, -65 ,-45, 71,
+                      -54 ,10 ,79 ,23 ,125 ,81 ,32 ,-17 ,-89, 90,
+                     119 ,124 ,226 ,222 ,102 ,92 ,92 ,134 ,182 ,192};
+
+    // derivitives of q at the endpoints
+    double t_0[3] = {-1236, 538, 42};
+    double  t_9[3] = {732, 1130, 63};
+
+    // generate controll points:
+    // p0
+    idx = 0;
+    double P[12*3];
+    for (uint i = 0; i < d; i++){
+        P[i*(n+2+1)+idx] = q[i*(n+1)+idx];
+        cout << P[i*(n+2+1)+idx] << ", ";
+    }
+
+    cout << endl;
+
+    // p1:
+    idx = 1;
+    for (uint i = 0; i < d; i++){
+        P[i*(n+2+1)+idx] = q[i*(n+1)+0]+(U[4]/3.0)*t_0[i];
+        cout << P[i*(n+2+1)+idx] << ", ";
+    }
+    cout << endl;
+
+    // p n+2:
+    idx = n+2;
+    for (uint i = 0; i < d; i++){
+        P[i*(n+2+1)+(n+2)] = q[i*(n+1)+n];
+        cout << P[i*(n+2+1)+(n+2)] << ", ";
+    }
+    cout << endl;
+
+
+//    // p n+1:
+//    idx = n+1;
+    idx = n+2;
+    for (uint i = 0; i < d; i++){
+        P[i*(n+2+1)+(n+1)] = q[i*(n+1)+n] - ((1-U[n+3])/3.0)*t_9[i];
+        cout << P[i*(n+2+1)+(n+1)] << ", ";
+    }
+    cout << endl;
+return 0;
+
 
     int n_knot = sizeof(U)/sizeof(U[0]) - 1;
-    cout << "n_knot " << n_knot << endl;
-    int p = 3, d = 3;
 
 
     for (double u = 0; u <=1; u+=0.01)
@@ -34,25 +84,11 @@ int main()
     {
 
         double s[p];
-
         BSplinePoint(u, U, n_knot, p, P, d, s);
 
-
         //for (uint i = 0; i < p; i++)
-             cout << s[2] <<  ", " ;
-
-
-
-        //int i = WhichSpan(u, U, n_knot, p);
-
-       // double B[p];
-
-       // BasisFuns(i, u, p, U, B);
-
-       // for (uint i = 0; i < p; i++)
-         //   cout << B[2] <<  ", " ;
-
-        cout << endl;
+       //      cout << s[2] <<  ", " ;
+        //cout << endl;
 
     }
 
@@ -76,7 +112,7 @@ P[]
 d
 - dimensions of a control point (2 in 2D, 3 in 3D, etc.)
 Output:s[]
-- value of the B-spline at u
+- value of the B-spline at uthe
 */
 {
     double B[MAX_P];
